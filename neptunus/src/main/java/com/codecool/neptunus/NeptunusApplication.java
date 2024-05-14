2aeb858db5f8bd8ca971536a805e237c476e5d80
Nagymengyi Reka <144277770+nagymengyireka@users.noreply.github.com>
2024-05-14 14:09:25 +0200

    initialised backend of the project

---
+++ neptunus/src/main/java/com/codecool/neptunus/NeptunusApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.neptunus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeptunusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeptunusApplication.class, args);
+    }
+
+}
---
+++ neptunus/src/test/java/com/codecool/neptunus/NeptunusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.neptunus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeptunusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
