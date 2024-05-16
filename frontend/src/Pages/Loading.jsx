261b905ce951806a38d1e8203ea5c980edacee07
whowful <konkoly.gergo@gmail.com>
2024-05-16 13:58:42 +0200

    syntax modifications

--- frontend/src/Pages/Loading.jsx
+++ frontend/src/Pages/Loading.jsx
@@ -1,7 +1,7 @@
 const Loading = () => (
-  <div class="d-flex justify-content-center">
-    <div class="spinner-border" role="status">
-      <span class="visually-hidden">Loading...</span>
+  <div className="d-flex justify-content-center">
+    <div className="spinner-border" role="status">
+      <span className="visually-hidden">Loading...</span>
     </div>
   </div>
 )
