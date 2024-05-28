0e3cc36df2605dbde171f549d8cb852f9187d396
whowful <konkoly.gergo@gmail.com>
2024-05-28 14:44:23 +0200

    add css for layout

--- frontend/src/Pages/Layout.jsx
+++ frontend/src/Pages/Layout.jsx
@@ -4,36 +4,62 @@ import { Link, Outlet } from 'react-router-dom';
 import "./Layout.css"
 
 const Layout = () => (
-<div>
-  <nav className="navbar navbar-expand-lg" style={{"background-color": "#BBF3EB"}}>
-    <ul className="navbar-nav flex-grow-1">
-      <li className="nav-item">
-        <a className="nav-link" href="/personal-info">
-          Personal Data
-        </a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="/taken-courses">
-          Taken Courses
-        </a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="/course-application">
-          Course Application
-        </a>
-      </li>
-    </ul>
-    <ul className="navbar-nav mx-auto p-2">
-      <li className="nav-item">
-        <button className="btn btn-outline-success custom-btn" type="button">
-          Log Out
-        </button>
-      </li>
-    </ul>
-  </nav>
-  <Outlet />
-</div>
-
+  <>
+    <div className='layoutHeader' style={{
+      "margin": "10px",
+    }}>
+      <img src='public/icon_v2.png' className='img-fluid' alt='logo' style={{
+        "width": "150px"
+      }}/>
+      <span className='h1' style={{
+        "fontFamily": "emoji"
+      }}>Neptunus</span>
+    </div>
+    <div>
+      <nav className="navbar navbar-expand-lg" style={{
+        "backgroundColor": "#E1E1E1",
+        "borderRadius": "10px",
+        "margin": "10px",
+        "padding": "10px",
+        "boxShadow": "rgba(0, 0, 0, 0.35) 0px 5px 15px"
+      }}>
+        <ul className="navbar-nav flex-grow-1">
+          <li className="nav-item" style={{
+            "borderRight": "solid 1px",
+            "borderColor": "#BFBFBF"
+          }}>
+            <a className="nav-link" href="/personal-info">
+              Personal Data
+            </a>
+          </li>
+          <li className="nav-item" style={{
+            "borderRight": "solid 1px",
+            "borderColor": "#BFBFBF"
+          }}>
+            <a className="nav-link border-right border-black" href="/taken-courses">
+              Taken Courses
+            </a>
+          </li>
+          <li className="nav-item" style={{
+            "borderRight": "solid 1px",
+            "borderColor": "#BFBFBF"
+          }}>
+            <a className="nav-link" href="/course-application">
+              Course Application
+            </a>
+          </li>
+        </ul>
+        <ul className="navbar-nav mx-auto p-2">
+          <li className="nav-item">
+            <button className="btn btn-outline-secondary" type="button">
+              Log Out
+            </button>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
+    </div>
+  </>
 );
 
 export default Layout;
