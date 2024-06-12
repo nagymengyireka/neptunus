3b9f1b2252eafc151b56e8319585ab2a80bbd9f2
Nagymengyi Reka <144277770+nagymengyireka@users.noreply.github.com>
2024-06-12 13:55:33 +0200

    created register page for teachers

---
+++ frontend/src/Pages/RegisterTeacher.jsx
@@ -0,0 +1,109 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Form, Modal, Button } from "react-bootstrap";
+
+function RegisterTeacher () {
+    const navigate = useNavigate();
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [name, setName] = useState('');
+    const [dateOfBirth, setDateOfBirth] = useState('');
+    const [gender, setGender] = useState('');
+
+    const [showModal, setShowModal] = useState(false);
+    const [modalMessage, setModalMessage] = useState('');
+    const [isSuccessful, setIsSuccessful] = useState(false);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const response = await fetch("/api/user/register/teacher", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                username,
+                password,
+                name,
+                dateOfBirth,
+                gender
+            })
+        });
+
+        if (response.ok) {
+            setModalMessage(`You successfully created a teacher account!`);
+            setIsSuccessful(true);
+        } else {
+            setModalMessage("Oh, oh. An unexpected error has occurred. Try again later.");
+            setIsSuccessful(false);
+        }
+
+        setShowModal(true);
+    }
+
+    const handleModalClose = () => {
+        setShowModal(false);
+        if (isSuccessful) {
+            navigate("/login/teacher");
+        } else {
+            navigate("/main");
+        }
+    }
+
+    return (
+        <>
+            <Form onSubmit={handleSubmit}>
+                <div className="row">
+                    <div className="col">
+                        <label htmlFor="name">Name</label>
+                        <input type="text" className="form-control" id="name" placeholder="Enter your name" value={name} onChange={(e) => setName(e.target.value)} required />
+                    </div>
+                    <div className="col">
+                        <label htmlFor="username">Username</label>
+                        <input type="text" className="form-control" id="username" placeholder="Choose a username" value={username} onChange={(e) => setUsername(e.target.value)} required />
+                    </div>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" className="form-control" id="password" placeholder="Enter password" value={password} onChange={(e) => setPassword(e.target.value)} minLength={6} required />
+                    <small id="passwordHelp" className="form-text text-muted">Must be at least 6 characters long.</small>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="dob">Date of Birth</label>
+                    <input type="date" className="form-control" id="dob" value={dateOfBirth} onChange={(e) => setDateOfBirth(e.target.value)} required />
+                </div>
+                <div className="form-check form-check-inline">
+                    <input className="form-check-input" type="radio" name="gender" id="gender1" value="MALE" checked={gender === "MALE"} onChange={(e) => setGender(e.target.value)} required />
+                    <label className="form-check-label" htmlFor="gender1">
+                        Male
+                    </label>
+                </div>
+                <div className="form-check form-check-inline">
+                    <input className="form-check-input" type="radio" name="gender" id="gender2" value="FEMALE" checked={gender === "FEMALE"} onChange={(e) => setGender(e.target.value)} required />
+                    <label className="form-check-label" htmlFor="gender2">
+                        Female
+                    </label>
+                </div>
+                <div className="form-group">
+                    <button type="submit">Create Teacher Account</button>
+                </div>
+            </Form>
+
+            <Modal show={showModal} onHide={handleModalClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Account Creation Status</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>{modalMessage}</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="primary" onClick={handleModalClose}>
+                        OK
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    )
+}
+
+export default RegisterTeacher;
